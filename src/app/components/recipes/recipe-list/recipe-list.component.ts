53db9f66e7aedba37f5c511974a2d2cf150ac1aa
Vaibhav Vaja <bulla@Vaibhavs-MacBook-Air.local>
2024-04-21 10:58:45 +0100

    recipes to be displayed from service

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
 
-  recipes : Recipe[] = [
-    new Recipe('Pesto Pasta' , 'Tasty Italian pasta with pesto sauce' , 'https://richanddelish.com/wp-content/uploads/2023/02/creamy-pesto-pasta-1.jpg'),
-    new Recipe('Red Pasta' , 'Tasty Italian pasta with Red sauce' , 'https://www.theburntbuttertable.com/wp-content/uploads/2022/03/roast-tomato-pasta-sauce-2.jpg'),
-    new Recipe('Pink Pasta' , 'Tasty Italian pasta with pink sauce' , 'https://beatthebudget.com/wp-content/uploads/2022/06/Pink-Sauce-Pasta-Tiktok-Pasta-featured-image-1200-x-1500px.jpg'),
-  ];
+  recipes : Recipe[];
 
   @Output() selectedRecipe = new EventEmitter<Recipe>();
+
+  constructor(private recipeService: RecipeService){}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes()
+  }
 
   OnRecipeSelected(event : Recipe) {
       this.selectedRecipe.emit(event);
--- src/app/components/recipes/recipe.service.ts
+++ src/app/components/recipes/recipe.service.ts
@@ -1,3 +1,26 @@
-export class RecipeService{
-    
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Pesto Pasta',
+      'Tasty Italian pasta with pesto sauce',
+      'https://richanddelish.com/wp-content/uploads/2023/02/creamy-pesto-pasta-1.jpg'
+    ),
+    new Recipe(
+      'Red Pasta',
+      'Tasty Italian pasta with Red sauce',
+      'https://www.theburntbuttertable.com/wp-content/uploads/2022/03/roast-tomato-pasta-sauce-2.jpg'
+    ),
+    new Recipe(
+      'Pink Pasta',
+      'Tasty Italian pasta with pink sauce',
+      'https://beatthebudget.com/wp-content/uploads/2022/06/Pink-Sauce-Pasta-Tiktok-Pasta-featured-image-1200-x-1500px.jpg'
+    ),
+  ];
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+  
 }
